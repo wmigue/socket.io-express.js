98f7a5a35fba1dfeaa81bdcb3d052959d228f931
unknown <wmigue@gmail.com>
2021-12-07 11:11:37 -0300

    7-12-2021

--- src/index.js
+++ src/index.js
@@ -14,24 +14,25 @@ const ArrNotas = []
 //servir todo lo del directorio public.
 app.use(express.static(__dirname + '/public'))
 
-//cada que alguien se conecte o refresque navegador
+//cada que alguien se conecte o refresque navegador.
 io.on('connection', (socket) => {
 
     console.log('nueva conexion desde cliente ID: ' + socket.id)
 
-    socket.emit('server:loadnotes', ArrNotas)
+    socket.emit('server:loadnotes', ArrNotas) //envio el array a la interfaz de los usuarios.
 
-    socket.on('cliente:newnote', (data) => {
+    socket.on('cliente:newnote', (data) => {  //cuando envio el formulario con submit
         const nota = {
             title: data.title,
             description: data.description,
             id: uuid()
         }     
-        ArrNotas.push(nota)
-        io.emit('server:newnote', nota) 
+        ArrNotas.push(nota) // agrego nota al array base.
+        io.emit('server:newnote', nota) //emito esta nota a todos los clientes conectados. (con socket.emit solo enviaria a ese socket o cliente individual, en cambio con io.emit envio a todos los sockets conectados.)
     })
 
 })
+
 
 server.listen(3000)
 console.log("server on port", 3000)
